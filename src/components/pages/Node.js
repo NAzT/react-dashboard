635e0256efa84af3564ee39798273a093a4f09c9
Nat <nat@cmmc.io>
2017-12-18 20:30:04 +0700

    remove unused.

--- src/components/pages/Node.js
+++ src/components/pages/Node.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Menu from '../menu/Menu.js'
 import LineMultiAxis from '../charts/LineMultiAxis.jsx'
 import Gauge from '../charts/Gauge.jsx'
-import store from '../../data/SensorStore'
+import sensorStore from '../../data/SensorStore'
 import mqttStore from '../../data/MqttStore'
 import _ from 'underscore'
 
@@ -11,22 +11,21 @@ export default class NodeTemplate extends Component {
 
   constructor (props) {
     super(props)
-    this.multi_line_charts = {}
     this.gauge = {
       temperature: 0,
       humidity: 0,
       pm10: 0,
-      pm25: 0,
+      pm2_5: 0,
     }
     this.state = {
       nodes: [],
       loading: true,
-      masterMenuItems: [],
       group2: [],
       sensorData: {
         multichart: {
           labels: ['pm1', 'pm2.5', 'pm10'],
-          data: [0, 0, 0]
+          data: [0, 0, 0],
+          x_labels: [0, 0, 0]
         },
       }
     }
@@ -35,54 +34,41 @@ export default class NodeTemplate extends Component {
   componentWillReceiveProps (nextProps) {
     this.page_id = nextProps.match.params.id
     this.gauge = mqttStore.state[this.page_id]
+    console.log(this.gauge)
   }
 
   _processStore = () => {
-    const station = store.sensor_stations[this.page_id]
+    const station = sensorStore.sensor_stations[this.page_id]
     if (station) {
-      const pm1 = station.results[0].series[0].values.map((v) => v[1])
-      const pm2_5 = station.results[0].series[0].values.map((v) => v[2])
-      const pm10 = station.results[0].series[0].values.map((v) => v[3])
-      this.setState((prevState, props) => {
-        return {
-          sensorData: {
-            multichart: Object.assign({}, {data: [pm1, pm2_5, pm10], labels: ['pm1', 'pm2.5', 'pm10']})
-          }
-        }
-      })
+      this.state.sensorData.x_labels = station.results[0].series[0].values.map((v) => v[0])
+      this.state.sensorData.pm1 = station.results[0].series[0].values.map((v) => v[1])
+      this.state.sensorData.pm2_5 = station.results[0].series[0].values.map((v) => v[2])
+      this.state.sensorData.pm10 = station.results[0].series[0].values.map((v) => v[3])
       this.updateGraphCache()
+      this._drawChart()
     }
   }
 
   componentWillMount () {
     this.page_id = this.props.match.params.id
-    console.log(`will mount with page_id => ${this.page_id}`)
-    this._processStore()
     mqttStore.addListener(() => {
       this.gauge = mqttStore.state[this.page_id]
       this._drawGauge()
     })
-    store.addListener(() => {
+    sensorStore.addListener(() => {
       this._processStore()
     })
+    this._processStore()
   }
 
   updateGraphCache = function () {
     this.data = this.state.sensorData.multichart
-    const station = store.sensor_stations[this.page_id]
+    const station = sensorStore.sensor_stations[this.page_id]
     if (station) {
       if (station.results[0].series === undefined) {
         console.error('data not found.')
       }
-      else {
-        const pm1 = station.results[0].series[0].values.map((v) => v[1])
-        const pm2_5 = station.results[0].series[0].values.map((v) => v[2])
-        const pm10 = station.results[0].series[0].values.map((v) => v[3])
-        this.multi_line_charts = this.state.sensorData.multichart
-        this.multi_line_charts.data[0] = pm1
-        this.multi_line_charts.data[1] = pm2_5
-        this.multi_line_charts.data[2] = pm10
-      }
+      else { }
     }
     else {
       console.error('station not found.. with id', this.props.match.params)
@@ -102,20 +88,13 @@ export default class NodeTemplate extends Component {
   }
 
   _drawGauge () {
-    ReactDOM.render(<Gauge
-        label='อุณหภูมิ'
-        symbol='℃' value={this.gauge.temperature}/>,
-      document.getElementById('temperature-g'))
-    ReactDOM.render(<Gauge label='ความชื้น'
-                           redraw
-                           symbol='%'
+    console.log(this.gauge)
+    ReactDOM.render(<Gauge label='อุณหภูมิ' symbol='℃'
+                           value={this.gauge.temperature}/>, document.getElementById('temperature-g'))
+    ReactDOM.render(<Gauge label='ความชื้น' redraw symbol='%'
                            value={this.gauge.humidity}/>, document.getElementById('humidity-g'))
-    ReactDOM.render(<Gauge label='PM10'
-                           symbol='ppm'
-                           value={this.gauge.pm10}/>, document.getElementById('pm10-g'))
-    ReactDOM.render(<Gauge label='PM2.5'
-                           symbol='ppm'
-                           value={this.gauge.pm25}/>, document.getElementById('pm25-g'))
+    ReactDOM.render(<Gauge label='PM10' symbol='µg/m3' value={this.gauge.pm10}/>, document.getElementById('pm10-g'))
+    ReactDOM.render(<Gauge label='PM2.5' symbol='µg/m3' value={this.gauge.pm25}/>, document.getElementById('pm2_5-g'))
   }
 
   componentDidUpdate () {
@@ -165,7 +144,7 @@ export default class NodeTemplate extends Component {
                     <div id='temperature-g' style={{width: '100%'}}/>
                     <div id='humidity-g' style={{width: '100%'}}/>
                     <div id='pm10-g' style={{width: '100%'}}/>
-                    <div id='pm25-g' style={{width: '100%'}}/>
+                    <div id='pm2_5-g' style={{width: '100%'}}/>
                   </div>
                   {/*<div id='masterGauge' className={this._ctxClassName('columns')}*/}
                   {/*style={{width: '100%'}}/>*/}
